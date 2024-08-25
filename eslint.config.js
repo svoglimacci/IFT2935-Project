2046f8657ac25e407273374ca3d74ba270851325
Simon Voglimacci <s.voglimacci@tuta.io>
2024-08-25 14:23:11 -0400

    initial commit

---
+++ eslint.config.js
@@ -0,0 +1,27 @@
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import pluginReact from 'eslint-plugin-react';
+import eslintConfigPrettier from 'eslint-config-prettier';
+import vitest from 'eslint-plugin-vitest';
+import jsxA11y from 'eslint-plugin-jsx-a11y';
+
+export default [
+  { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  eslintConfigPrettier,
+  jsxA11y.flatConfigs.recommended,
+  {
+    files: ['tests/**'], // or any other pattern
+    plugins: {
+      vitest,
+    },
+    rules: {
+      ...vitest.configs.recommended.rules, // you can also use vitest.configs.all.rules to enable all rules
+      'vitest/max-nested-describe': ['error', { max: 3 }], // you can also modify rules' behavior using option like this
+    },
+  },
+];
---
+++ vite.config.ts
@@ -0,0 +1,32 @@
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vitest/config';
+import viteTsconfigPaths from 'vite-tsconfig-paths';
+
+export default defineConfig({
+  base: './',
+  plugins: [react(), viteTsconfigPaths()],
+  server: {
+    port: 3000,
+  },
+  preview: {
+    port: 3000,
+  },
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/testing/setup-tests.ts',
+    exclude: ['**/node_modules/**', '**/e2e/**'],
+    coverage: {
+      include: ['src/**'],
+    },
+  },
+  optimizeDeps: { exclude: ['fsevents'] },
+  build: {
+    rollupOptions: {
+      external: ['fs/promises'],
+      output: {
+        experimentalMinChunkSize: 3500,
+      },
+    },
+  },
+});
