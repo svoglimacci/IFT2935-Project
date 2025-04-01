bb6447a815735deecd53ab4f76f24339cf5320d3
Simon Voglimacci <s.voglimacci@tuta.io>
2025-04-01 13:37:52 -0400

    feat: complete base UI

--- server/routes.js
+++ server/routes.js
@@ -12,4 +12,24 @@ router.get('/users', async (req, res) => {
   }
 });
 
+router.get('/users/:id', async (req, res) => {
+  const userId = parseInt(req.params.id, 10);
+  if (isNaN(userId)) {
+    return res.status(400).json({ message: 'Invalid user ID' });
+  }
+
+  try {
+    const result = await req.pool.query('SELECT * FROM users WHERE id = $1', [
+      userId,
+    ]);
+    if (result.rows.length === 0) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+    res.json(result.rows[0]);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Internal Server Error' });
+  }
+});
+
 export default router;
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,8 +1,6 @@
 import React, { JSX, ReactNode } from 'react';
 import classes from './Header.module.css';
 import ThemeSwitcher from '../ThemeSwitcher/ThemeSwitcher';
-import NavLink from '../NavLink/NavLink';
-
 export type HeaderProps = {
   children?: ReactNode;
 };
@@ -11,12 +9,7 @@ const Header = (): JSX.Element => {
   return (
     <header className={classes.header}>
       <h1>IFT235 - Projet</h1>
-      <nav className={classes.navList}>
-        <NavLink to="/"> Home</NavLink>
-        <NavLink to="/1"> Page 1</NavLink>
-        <NavLink to="/2"> Page 2</NavLink>
-        <NavLink to="/3"> Page 3</NavLink>
-      </nav>
+      <h2>Bibliothèque</h2>
       <ThemeSwitcher />
     </header>
   );
